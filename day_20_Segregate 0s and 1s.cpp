641bf8a5d2f62ca29fc4243fe8955768cdd531c5
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-13 20:05:07 +0000

    day-20

---
+++ day_20_Segregate 0s and 1s.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+// Initial template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    void segregate0and1(vector<int> &arr) {
+        // code here
+        int cnt = 0;
+        for(int i = 0; i< arr.size(); i++)
+        {
+            if(arr[i]==0) cnt++;
+        }
+        int ind=0;
+        for(;ind<cnt;ind++)
+        arr[ind]=0;
+        for(;ind<arr.size();ind++) arr[ind]=1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n = arr.size();
+        Solution ob;
+        ob.segregate0and1(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
