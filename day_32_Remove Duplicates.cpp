e2b036c235efd06ca725400b706699e06765965e
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-07-28 07:56:27 +0000

    day 32

---
+++ day_32_Remove Duplicates.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+        string ans = "";
+        bool temp[26];
+        for ( auto x: str) {
+            if(!temp[x-'a']) {
+                ans += x;
+                temp[x-'a'] = true;
+            }
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
