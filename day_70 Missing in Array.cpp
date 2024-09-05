d9e910c757cf72efea1748b3ce23bb92556b1916
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-09-05 21:08:41 +0530

    Create day_70 Missing in Array.cpp

---
+++ day_70 Missing in Array.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    // Note that the size of the array is n-1
+    int missingNumber(int n, vector<int>& array) {
+
+        // Your code goes here
+         int a=n;
+        int sum1=0;
+        int sum=(a*(a+1))/2;
+        for(int i=0;i<array.size();i++)
+        {
+            sum1=sum1+array[i];
+        }
+        return sum-sum1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> arr(n - 1);
+        for (int i = 0; i < n - 1; ++i)
+            cin >> arr[i];
+        Solution obj;
+        cout << obj.missingNumber(n, arr) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
