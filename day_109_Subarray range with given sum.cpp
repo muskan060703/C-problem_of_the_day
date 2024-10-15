f85d0f782d5c718b33b22b70b8fa9134c9daa082
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-10-15 22:32:15 +0530

    Create day_109_Subarray range with given sum.cpp

---
+++ day_109_Subarray range with given sum.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int target) {
+        // Your code here
+        int ans = 0, sum = 0;
+        unordered_map<int,int>mm;
+        for(int x : arr)
+        {
+            sum += x;
+            if(sum == target) ans++;
+            if(mm[sum-target]) ans+=mm[sum-target];
+            mm[sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
