53ba6f79eaeeed6c54158b14ff56d0854693d4f5
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-10-25 08:50:41 +0530

    Create day_119_Alternative Sorting.cpp

---
+++ day_119_Alternative Sorting.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+
+  public:
+    vector<int> alternateSort(vector<int>& arr) {
+        // Your code goes here
+        sort(arr.begin(),arr.end());
+        vector<int>ans;
+        int n = arr.size();
+        int start = 0;
+        int end = n-1;
+        if(n%2==1)
+        {
+        while(start < end )
+        {
+            ans.push_back(arr[end]);
+            ans.push_back(arr[start]);
+            start++;
+            end--;
+        }
+        ans.push_back(arr[start]);
+        }
+        else
+        {
+           while(start <= end )
+        {
+            ans.push_back(arr[end]);
+            ans.push_back(arr[start]);
+            start++;
+            end--;
+        } 
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution obj;
+        vector<int> ans = obj.alternateSort(arr);
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
