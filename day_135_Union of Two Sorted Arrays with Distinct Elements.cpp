4e2dd5e0b1b70db843b17f9326c08f74b900fabd
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-11-10 21:48:08 +0530

    Create day_135_Union of Two Sorted Arrays with Distinct Elements.cpp

---
+++ day_135_Union of Two Sorted Arrays with Distinct Elements.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        vector<int>ans;
+        unordered_set<int>s;
+        for(int i = 0; i < a.size(); i++) {
+            if(s.find(a[i]) == s.end())
+            {
+                ans.push_back(a[i]);
+            }
+            s.insert(a[i]);
+        }
+        for(int i = 0; i < b.size(); i++) {
+            if(s.find(b[i]) == s.end())
+            {
+                ans.push_back(b[i]);
+            }
+            s.insert(b[i]);
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> a, b;
+        string input;
+
+        // Read first array
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            a.push_back(number);
+        }
+
+        // Read second array
+        getline(cin, input);
+        stringstream ss2(input);
+        while (ss2 >> number) {
+            b.push_back(number);
+        }
+
+        Solution ob;
+        vector<int> ans = ob.findUnion(a, b);
+        for (int i : ans)
+            cout << i << ' ';
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
